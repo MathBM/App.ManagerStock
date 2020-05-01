8e34c60f432d6996be51de490c076d2c07e6fb10
Matheus <58346368+MBMunzi@users.noreply.github.com>
2020-05-01 01:08:15 -0300

    Login Window with Validation

--- signin.py
+++ signin.py
@@ -3,7 +3,24 @@ from kivy.uix.boxlayout import BoxLayout
 
 
 class SigninWindow(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def validate_user(self):
+        user = self.ids.username_field
+        pwd = self.ids.pwd_field
+
+        uname = user.text
+        passw = pwd.text
+        info = self.ids.info
+
+        if uname == '' or passw == '':
+            info.text = '[color=#FF0000]Username and password required.[/color]'
+        else:
+            if uname == 'admin' and passw == 'admin':
+                info.text = '[color=#00FF00]Logged In Successfully!!![/color]'
+            else:
+                info.text = '[color=#FF0000]Invalid Password or Username.[/color]'
 
 
 class SigninApp(App):
